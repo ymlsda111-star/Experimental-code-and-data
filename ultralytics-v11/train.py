4d8e9f00a36626ed00e1f680eee5482870d43a0a
ymlsda111-star <ymlsda111@gmail.com>
2025-11-08 14:14:22 +0800

    Refactor training script for YOLO model

--- ultralytics-v11/train.py
+++ ultralytics-v11/train.py
@@ -14,20 +14,6 @@ def main():
         batch=16,                 # 批次大小 if报CUDA out of memory，需降低 batch 或 imgsz
         imgsz=640,                # 图像尺寸 640 * 640检测小目标但增加显存占用+高性能场景
         device='0',               # GPU（如 '0' 或 'cpu'）cpu速度可能会慢50-100倍
-
-        # 数据增强参数
-        #hsv_h=0.015,  # 色调增强 (0.0-1.0)
-        #hsv_s=0.7,  # 饱和度增强 (0.0-1.0)
-        #hsv_v=0.4,  # 明度增强 (0.0-1.0)
-       # degrees=45.0,  # 旋转角度 (-180 to 180)
-        #translate=0.1,  # 平移 (0.0-1.0)
-        #scale=0.5,  # 缩放比例
-        #shear=0.0,  # 剪切角度
-        #perspective=0.0,  # 透视变换
-        #flipud=0.0,  # 上下翻转概率
-        #fliplr=0.5,  # 左右翻转概率
-
-        #width=0.25,
         name='yolov11_litch',     #保存训练结果的文件夹名称
         save=True,                 # 是否保存训练过程和结果
     )
@@ -36,5 +22,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
